8c17a9ef454f2f538154929bccd3036063f54e3c
shiqyn <shiqyn@ShiqyndeMacBook-Pro-15-inch.local>
2015-05-19 12:39:03 +0800

    fix mjalertview

--- STCommonUtil/UIView+MJAlertView.m
+++ STCommonUtil/UIView+MJAlertView.m
@@ -7,10 +7,10 @@
 //
 
 #import "UIView+MJAlertView.h"
-#import "AppDelegate.h"
+
 #import <QuartzCore/QuartzCore.h>
 
-#define APPDELEGATE ((AppDelegate *)[UIApplication sharedApplication].delegate)
+#define APPDELEGATE ([UIApplication sharedApplication].delegate)
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 #define NOTIFIER_LABEL_FONT ([UIFont fontWithName:@"HelveticaNeue-Light" size:18])
